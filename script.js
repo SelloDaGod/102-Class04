7256a0a672571e5007e6c90d65735891751d19fa
SelloDaGod <Marcel.mk1577@gmail.com>
2023-02-09 04:53:34 -0600

    added code for lab 05

---
+++ script.js
@@ -0,0 +1,18 @@
+let apple = 1;
+
+function applepie() {
+    var ingredientX = 5;
+    console.log(apple)
+}
+let pie2 = function(cinnamon) {
+    console.log(apple+cinnamon);
+}
+applepie();
+pie2(9);
+console.log(ingredientX);
+
+if(myname != 'Sello'){
+    alert('error hacker error');
+} else {
+    alert('hello')
+}
